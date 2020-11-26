1c7a68c19a274d9471f85c52af4018d97cf42895
angel <angel@SurfaceBook2>
2020-11-26 14:03:26 +0000

    Version 3 Kata1
    Usamos LocalDate y Period

--- src/kata1/Main.java
+++ src/kata1/Main.java
@@ -1,8 +1,6 @@
 package kata1;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
 
 /**
  * Crear una clase Person para el cálculo de la edad.
@@ -13,19 +11,13 @@ import java.util.GregorianCalendar;
 public class Main {
 
     public static void main(String[] args) {
-        
-        //Person person = new Person("Angel", new Date(99,8,24)); 
-        Calendar date = GregorianCalendar.getInstance();
-        date.set(1999,8,24);
-        
-        Calendar date2 = GregorianCalendar.getInstance();
-        date2.set(1997,11,24);
-        
+
+        LocalDate date = LocalDate.of(1999, 11, 24);
+
         Person person = new Person("Angel",date);
-        System.out.println(person.getName() + " tiene " + person.getAge() + " años");
+        System.out.println(person.getName() + " nacio el " + person.getBirthdate()
+                + " y tiene " + person.getAge() + " años");
     
-        Person person2 = new Person("Pepe",date2);
-        System.out.println(person2.getName() + " tiene " + person2.getAge() + " años");
     }
     
     
